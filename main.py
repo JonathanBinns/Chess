0a3a7c7ba58f013498a60d83ca0df1baa63a8f3c
Jonthelepreeedcchaun <jonathanmichaelbinns@gmail.com>
2022-07-25 21:56:08 -0700

    testing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ window = windowClass()
 game = gameClass()
 gameState = "menu"
 
+# the main loop of the program
 while window.isRunning():
     window.processing()
     window.screen.fill((90, 70, 50))
